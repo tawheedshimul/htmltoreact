f5d0586c8286ce26f106d1241bfa303ca505ab84
unknown <you@example.com>
2024-01-19 18:24:23 +0600

    update

--- src/Pages/MenuBar.jsx
+++ src/Pages/MenuBar.jsx
@@ -1,54 +1,53 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 function MenuBar() {
   return (
     <header>
-      <Navbar collapseOnSelect expand="lg" >
-        <Container>
-          <Navbar.Brand className='text-white' href="#home">GDPC COIN</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav" className='justify-content-end'>
-            <Nav>
-              <ul class="navbar-nav">
-                <li class="nav-item active">
-                  <a class="nav-link" href="#">Home</a>
+      <Navbar collapseOnSelect expand="lg" className='mx-5'>
+        <Navbar.Brand className='text-white' href="#home"><h6>GDPC COIN</h6></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav" className='justify-content-end'>
+          <Nav>
+            <div className='nav-border'>
+              <ul className="navbar-nav">
+                <li className="nav-item active">
+                  <a className="nav-link" href="#">Home</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#cheapest-sec">About</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#cheapest-sec">About</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#industry-leader">Patners</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#industry-leader">Partners</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#accordian-sec">Coin</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#accordian-sec">Coin</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#roadmapping">Roadmap </a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#roadmapping">Roadmap </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="./images/WPF_GDP_010124.pdf">Whitepaper</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="./images/WPF_GDP_010124.pdf">Whitepaper</a>
                 </li>
-
-                <li class="nav-item">
-                  <a class="nav-link" href="#contact">Contact </a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#contact">Contact </a>
                 </li>
-                <li class="nav-item language-switherss">
-                  <a class="nav-link" href="#">Languages</a>
-                  <ul class="language-swither">
+                <li className="nav-item language-switherss">
+                  <a className="nav-link" href="#">Languages</a>
+                  <ul className="language-swither">
                     <li><a href="#">English</a></li>
                     <li><a href="#">German</a></li>
                   </ul>
                 </li>
+                <div className="header-btn" style={{ height: '10px' }}>
+                  <a href="#">Connect Wallet</a>
+                </div>
               </ul>
-              <div class="d-flex header-btn">
-                <a href="#">Connect Wallet</a>
-              </div>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+            </div>
+
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </header>
   );
